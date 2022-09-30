9adbdf873e4bb1d1f04c432f3e0fe4c44ac6ca89
Felipe Bueno <felipecyano@gmail.com>
2022-09-30 10:42:55 -0300

    feat: implementa mapa com react-leaflet

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -3,6 +3,10 @@ import "../styles/pages/orphanages-map.css";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+
+// import "leaflet/dist/leaflet.css";
+
 import mapMarkerImg from "../images/map-marker.svg";
 
 function OrphanagesMap() {
@@ -22,7 +26,17 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <MapContainer 
+        center={[-23.6571683, -46.5223315]} 
+        zoom={13} 
+        style={{ width: '100%', height: '100%', zIndex: '5'}}
+        id="map"
+      >
+        <TileLayer 
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
