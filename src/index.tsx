74dfe726fd57d6b55ba8f9e4cdc271064854111c
FelipeCyano <felipecyano@gmail.com>
2022-12-22 10:14:56 -0300

    docs: remove comentários desnecessários.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
-/* 
-* primeiro arquivo 'javascript' carregado na aplicação. 
-*/
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // integração do React com a DOM.
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-// método .render() que recebe tags (componentes).
+
 root.render(
   <React.StrictMode>
     <App />
